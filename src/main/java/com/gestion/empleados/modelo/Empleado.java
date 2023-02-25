cc41e7aa888365ad4050cd9d012df1e0cbf12cc2
SpyasDev <79065468+fastleon@users.noreply.github.com>
2023-02-25 16:40:56 -0500

    Creacion de la entidad que nos sirve de modelo para la Base de datos

---
+++ src/main/java/com/gestion/empleados/modelo/Empleado.java
@@ -0,0 +1,38 @@
+package com.gestion.empleados.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empleados")
+public class Empleado {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "nombre", length = 60, nullable=false)
+    private String nombre;
+    
+    @Column(name = "apellido", length = 60, nullable=false)
+    private String apellido;
+    
+    @Column(name = "email", length = 60, nullable=false, unique = true)
+    private String email;
+    
+    public Empleado(){
+    }
+
+    public Empleado(Long id , String nombre , String apellido , String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+    
+    
+}

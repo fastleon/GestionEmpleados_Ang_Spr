ada64e83325fdfc40914970d526016874b3c1eae
SpyasDev <79065468+fastleon@users.noreply.github.com>
2023-02-25 17:04:27 -0500

    Creamos el repositorio de la entidad Empleado y empezamos con el controlador

--- src/main/java/com/gestion/empleados/excepciones/ResourceNotFoundException.java
+++ src/main/java/com/gestion/empleados/excepciones/ResourceNotFoundException.java
@@ -1,5 +1,16 @@
 package com.gestion.empleados.excepciones;
 
-public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    
+    private static final long serialVersionUID = 1L;
+    
+    public ResourceNotFoundException(String mensaje){
+        super(mensaje);
+    }
     
 }
+  
--- src/main/java/com/gestion/empleados/repositorio/EmpleadoRepositorio.java
+++ src/main/java/com/gestion/empleados/repositorio/EmpleadoRepositorio.java
@@ -1,5 +1,6 @@
 package com.gestion.empleados.repositorio;
 
+import com.gestion.empleados.modelo.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
